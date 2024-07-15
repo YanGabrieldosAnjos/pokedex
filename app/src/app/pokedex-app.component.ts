48a85edb61cdba847dc7c18a00fdfa27a317ae21
Yan <yangabrieldosanjos@gmail.com>
2024-07-14 23:03:00 -0300

    feat: main screen

--- app/src/app/pokedex-app.component.ts
+++ app/src/app/pokedex-app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Route, Router, RouterOutlet } from '@angular/router';
+import { PokemonService } from './pokemon/pokemon.service';
+import { IPokedexList } from '../../../api/src/pokemon/interface/pokedex.interface';
 
 @Component({
   selector: 'pokedex',
   templateUrl: './pokedex-app.component.html',
   styleUrl: './pokedex-app.component.scss'
 })
-export class PokedexAppComponent {}
+export class PokedexAppComponent implements OnInit{
+  pokemons: IPokedexList | null = null;
+  filteredItems = [];
+  constructor(private pokemonService: PokemonService, private router: Router) {}
+  
+  async ngOnInit(): Promise<void> {
+    this.pokemons = await this.pokemonService.getList();
+    console.log(this.pokemons);
+  }
+
+  async search(name?: string) {}
+  async details(id: string) {}
+}
--- app/src/app/pokemon/pokemon.service.ts
+++ app/src/app/pokemon/pokemon.service.ts
@@ -1,4 +1,23 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { lastValueFrom, Observable } from "rxjs";
+import { IPokedexList } from "../../../../api/src/pokemon/interface/pokedex.interface";
 
-@Injectable()
-export class PokemonService {}
+@Injectable({
+    providedIn: 'root',
+})
+export class PokemonService {
+    private apiUrl = 'http://localhost:3000/api';
+    constructor(private client: HttpClient) {}
+
+    async getList(pagination?: {limit: number, offset: number}, url?: string): Promise<IPokedexList> {
+        let query = this.apiUrl+ "/pokemon";
+        if(pagination) {
+            query += `/?limit=${pagination.limit}&offset=${pagination.offset}`
+        }
+        if(url) {
+            query += url;
+        }
+        return (await lastValueFrom(this.client.get<IPokedexList>(query)));
+    }
+}
